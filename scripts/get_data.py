3b1be721b93da6947be9edcc5f0b4bf8fe231bed
akrobi <gwangui00000@gmail.com>
2022-08-23 00:09:45 +0300

    calculate num of missing values in the orig data

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -4,6 +4,29 @@ from IPython.display import Image
 import warnings
 warnings.filterwarnings('ignore')
 
-# pd.set_option('max_column', None)
 db = pd.read_excel(r'~\\Documents\\projects\\10academy\\week1\\TellCoAnalysis\\data\\TellCo_financial_data.xlsx', na_values=['?', None])
-print(db.head())
+# print(db.head())
+
+# column names. works
+# print(db.columns.tolist())
+
+# how many missing values exist or better still what is the % of missing values in the dataset?
+def percent_missing(df):
+
+    # Calculate total number of cells in dataframe
+    totalCells = np.product(df.shape)
+
+    # Count number of missing values per column
+    missingCount = df.isnull().sum()
+
+    # Calculate total number of missing values
+    totalMissing = missingCount.sum()
+
+    # Calculate percentage of missing values
+    print("The TellCo dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
+
+percent_missing(db)
+
+# to see which columns have missing values
+print(db.isna().sum())
+
