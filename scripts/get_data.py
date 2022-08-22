5613a94a179cb9e7f65fff528f43e5416426a411
akrobi <gwangui00000@gmail.com>
2022-08-22 18:33:41 +0300

    not extracting data. file directory issue

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -4,7 +4,6 @@ from IPython.display import Image
 import warnings
 warnings.filterwarnings('ignore')
 
-pd.set_option('max_column', None)
-db = pd.read_csv('/content/drive/Shareddrives/10 Academy/Intensive training/Batch 6/Content-B6/Week-1/data/diabetic_data.csv', na_values=['?', None])
-# db = pd.read_csv('<path-to-your-csv-inside-your-drive>',  na_values=['?', None])
+# pd.set_option('max_column', None)
+db = pd.read_excel('../data/Week1_challenge_data_source.xlsx', na_values=['?', None])
 db.head()
